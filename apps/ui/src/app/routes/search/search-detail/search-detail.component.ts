ed4a3d8568362f7de4883608099ccd9e4ab0f967
Ross Carmack <carmackbooking@gmail.com>
2022-09-24 13:14:18 -0500

    added map and styling to search detail

--- apps/ui/src/app/routes/search/search-detail/search-detail.component.ts
+++ apps/ui/src/app/routes/search/search-detail/search-detail.component.ts
@@ -17,6 +17,18 @@ export interface DialogData {
   styleUrls: ['./search-detail.component.scss'],
 })
 export class SearchDetailComponent {
+
+  center: google.maps.LatLngLiteral = { lat: 32.821688, lng: -96.792936 };
+  options: google.maps.MapOptions = {
+    zoomControl: true,
+    mapTypeControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: false,
+    streetViewControl: false
+  };
+  markerPosition = {lat: 32.821688, lng: -96.792936}
+  zoom = 15;
+
   constructor(
     public dialogRef: MatDialogRef<SearchDetailComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
