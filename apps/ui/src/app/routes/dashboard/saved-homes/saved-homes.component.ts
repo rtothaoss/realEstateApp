76ef3e4e69064fdf07dd012885f8ef59c3b38f7d
Ross Carmack <carmackbooking@gmail.com>
2022-10-18 10:14:34 -0500

    added heart indicator on saved homes dashboard which allows a user to unsave a home from the dashboard

--- apps/ui/src/app/routes/dashboard/saved-homes/saved-homes.component.ts
+++ apps/ui/src/app/routes/dashboard/saved-homes/saved-homes.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PropertyDetail } from '@starter/api-interfaces';
 import { BehaviorSubject } from 'rxjs';
 import { formatWithOptions } from 'util';
 import { SearchService } from '../../../shared/services/search.service';
 import { SearchDetailComponent } from '../../search/search-detail/search-detail.component';
+
 
 @Component({
   selector: 'starter-saved-homes',
@@ -25,11 +26,18 @@ export class SavedHomesComponent implements OnInit {
   loadingSubject = new BehaviorSubject<boolean>(false);
   loading$ = this.loadingSubject.asObservable();
   propertyDetail!: PropertyDetail;
+  closedHeart = '../../../../assets/img/blueHeart.png'
+  deleting = false;
 
   constructor(public dialog: MatDialog, private searchService: SearchService) {}
 
   ngOnInit(): void {
+    this.savedHomesSub()
+  }
+
+  savedHomesSub() {
     this.searchService.savedHomes().subscribe((details) => {
+      console.log('this fired off')
       this.location = details;
       console.log(details);
       this.loadingSubject.next(true);
@@ -37,42 +45,59 @@ export class SavedHomesComponent implements OnInit {
   }
 
   openDialog(propertyID: string) {
-    console.log(propertyID)
-    this.searchService.propertyDetailApi(propertyID).subscribe((details) => {
-      console.log(details)
-      //put this in a seperate function to make things look cleaner
-      this.propertyDetail = details.data.property_detail;
-      const formattedAddress = `${this.propertyDetail.address.line}, ${this.propertyDetail.address.city}, ${this.propertyDetail.address.state_code} ${this.propertyDetail.address.postal_code}`;
-      const dialogRef = this.dialog.open(SearchDetailComponent, {
-        width: '1100px',
-        height: '900px',
-        data: {
-          address: formattedAddress,
-          photo: this.propertyDetail.photos[0].href,
-          price: this.propertyDetail.prop_common.price,
-          beds: this.propertyDetail.prop_common.bed,
-          bath: this.propertyDetail.prop_common.baths_full,
-          sqft: this.propertyDetail.prop_common.sqft,
-          marker: {
-            lat: this.propertyDetail.address.location.lat,
-            lng: this.propertyDetail.address.location.lon,
+    console.log(this.deleting)
+    console.log(!this.deleting)
+   
+    if(!this.deleting) {
+      console.log(this.deleting)
+      console.log(propertyID)
+      this.searchService.propertyDetailApi(propertyID).subscribe((details) => {
+        console.log(details)
+        //put this in a seperate function to make things look cleaner
+        this.propertyDetail = details.data.property_detail;
+        const formattedAddress = `${this.propertyDetail.address.line}, ${this.propertyDetail.address.city}, ${this.propertyDetail.address.state_code} ${this.propertyDetail.address.postal_code}`;
+        const dialogRef = this.dialog.open(SearchDetailComponent, {
+          width: '1100px',
+          height: '900px',
+          data: {
+            address: formattedAddress,
+            photo: this.propertyDetail.photos[0].href,
+            price: this.propertyDetail.prop_common.price,
+            beds: this.propertyDetail.prop_common.bed,
+            bath: this.propertyDetail.prop_common.baths_full,
+            sqft: this.propertyDetail.prop_common.sqft,
+            marker: {
+              lat: this.propertyDetail.address.location.lat,
+              lng: this.propertyDetail.address.location.lon,
+            },
+            overview: this.propertyDetail.prop_common.description,
+            photos: this.propertyDetail.photos,
+            propertyHistory: this.propertyDetail.property_history,
+            mortgageInfo: this.propertyDetail.mortgage.estimate,
+            schools: this.propertyDetail.schools,
+            propertyID: propertyID,
+            type: this.propertyDetail.display_property_type,
+            yearBuilt: this.propertyDetail.prop_common.year_built,
+            parking: this.propertyDetail.prop_common.garage,
+            lot: this.propertyDetail.prop_common.lot_sqft,
           },
-          overview: this.propertyDetail.prop_common.description,
-          photos: this.propertyDetail.photos,
-          propertyHistory: this.propertyDetail.property_history,
-          mortgageInfo: this.propertyDetail.mortgage.estimate,
-          schools: this.propertyDetail.schools,
-          propertyID: propertyID,
-          type: this.propertyDetail.display_property_type,
-          yearBuilt: this.propertyDetail.prop_common.year_built,
-          parking: this.propertyDetail.prop_common.garage,
-          lot: this.propertyDetail.prop_common.lot_sqft,
-        },
+        });
+  
+        dialogRef.afterClosed().subscribe((result) => {
+          console.log(result)
+          console.log('The dialog was closed');
+        });
       });
+    }
+ 
+  }
 
-      dialogRef.afterClosed().subscribe((result) => {
-        console.log('The dialog was closed');
-      });
+  unSave(propertyId: string) {
+    this.deleting = true;
+    this.searchService.deleteHome(propertyId).subscribe((details) => {
+      this.savedHomesSub()
+      this.deleting = false;
     });
   }
+
 }
