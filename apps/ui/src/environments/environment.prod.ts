c68288f96b75909d0776c4806f2c3167fd777221
Ross Carmack <carmackbooking@gmail.com>
2022-11-04 13:59:27 -0500

    add images to google markers

--- apps/ui/src/environments/environment.prod.ts
+++ apps/ui/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:3000/api',
-  googleMapsApiKey: 'AIzaSyB9hcvIilbYlBwiP_jeehvjKpiF3Vo7goM'
+  googleMapsApiKey: 'AIzaSyB9hcvIilbYlBwiP_jeehvjKpiF3Vo7goM',
+  realEstateApiKey: 'cf5fb7698cmsh458bbee859c8181p11d143jsnc5a80b9e6e22'
 };
