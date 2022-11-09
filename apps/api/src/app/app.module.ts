6251f543b60776491441edfde3a3a3a40ffe3de6
Ross Carmack <carmackbooking@gmail.com>
2022-11-08 18:57:22 -0600

    checking something in api app.module.ts

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { SearchModule } from './search/search.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
+console.log('-----------')
+console.log('this is dirname')
+console.log(__dirname)
+console.log('-----------')
+
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'starter'),
